510790e4c9f589914bd6ad9b86b55dc28f71f7bf
Abhirup Dutta <abhirupdutta@Abhirups-MacBook-Pro.local>
2025-11-11 00:30:29 -0500

    Delete functionality on tasks, done, and full-board.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const SEPARATOR = " ";
 
 const taskInput = document.getElementById("taskInput");
 const isPriority = document.getElementById("isPriority");
@@ -6,10 +7,13 @@ const columnList = document.getElementById("kanbanColumns").children;
 
 let draggedTask = null;
 
+/*
+ * Create Task
+ */
+
 function createTaskElement(text, isPriorityChecked, id = Date.now().toString()) {
     const taskDOMElement = document.createElement("div");
     taskDOMElement.id = id;
-    taskDOMElement.textContent = text;
     taskDOMElement.className = isPriorityChecked ? "kanban-card-priority" : "kanban-card";
     taskDOMElement.draggable = true;
 
@@ -28,29 +32,145 @@ function createTaskElement(text, isPriorityChecked, id = Date.now().toString()) 
         saveTasks_To_LocalStorage();
     });
 
+    add_Text_To_Task(text, taskDOMElement);
+    add_Delete_To_Task(taskDOMElement);
+
+    console.log(taskDOMElement);
     return taskDOMElement;
 }
 
+/*
+ * DELETE FROM LOCAL STORAGE
+ */
+
+function deleteTask_From_LocalStorage(taskId) {
+    console.log('deleting ' + taskId);
+    let savedTasks = JSON.parse(localStorage.getItem("tasks_in_localStorage"));
+    let tasksAfterDelete = savedTasks.filter(task => task.id !== taskId);
+    localStorage.setItem("tasks_in_localStorage", JSON.stringify(tasksAfterDelete));
+}
+
+function delete_Done_From_LocalStorage() {
+    let savedTasks = JSON.parse(localStorage.getItem("tasks_in_localStorage"));
+    let tasksAfterDelete = savedTasks.filter(task => task.columnId !== "doneColumn");
+    localStorage.setItem("tasks_in_localStorage", JSON.stringify(tasksAfterDelete));
+}
+
+function delete_All_from_LocalStorage() {
+    localStorage.removeItem("tasks_in_localStorage");
+}
+
+/*
+ * CREATE TEXT & DELETE MARKS
+ */
+
+function createTaskText(text, taskId) {
+    let deleteDOMElement = document.createElement("span");
+    deleteDOMElement.id = "text" + taskId;
+    deleteDOMElement.className = "task-text";
+    deleteDOMElement.textContent = text + SEPARATOR;
+    return deleteDOMElement;
+}
+
+function createDeleteMark(taskId) {
+    let deleteDOMElement = document.createElement("span");
+    deleteDOMElement.id = "delete" + taskId;
+    deleteDOMElement.className = "delete-mark";
+    deleteDOMElement.textContent = "(X)";
+    deleteDOMElement.addEventListener("click", (e) => {
+        document.getElementById(taskId).remove();
+        deleteTask_From_LocalStorage(taskId);
+    });
+    return deleteDOMElement;
+}
+
+function createDeleteDoneMark() {
+    let deleteDOMElement = document.createElement("span");
+    deleteDOMElement.id = "deleteDone";
+    deleteDOMElement.className = "delete-mark";
+    deleteDOMElement.textContent = "(X)";
+    deleteDOMElement.addEventListener("click", (e) => {
+
+        let tasksDone = document.getElementById("doneColumn").children;
+        Array.from(tasksDone).filter(task => task.className !== "column-title-done")
+            .forEach((task) => { task.remove(); });
+
+        delete_Done_From_LocalStorage();
+
+        alert("Cleared all tasks done.");
+    });
+    return deleteDOMElement;
+}
+
+function createDeleteAllMark() {
+    let deleteDOMElement = document.createElement("span");
+    deleteDOMElement.id = "deleteAll";
+    deleteDOMElement.className = "delete-mark";
+    deleteDOMElement.textContent = "(X)";
+    deleteDOMElement.addEventListener("click", (e) => {
+        let isDeleteConfirmed = confirm("Are you sure you want to delete all tasks on all boards?");
+        if (isDeleteConfirmed) {
+            let tasksOnPage = document.querySelectorAll(".kanban-card");
+            tasksOnPage.forEach(task => { task.remove(); });
+            tasksOnPage = document.querySelectorAll(".kanban-card-priority");
+            tasksOnPage.forEach(task => { task.remove(); });
+
+            delete_All_from_LocalStorage();
+        }
+    });
+    return deleteDOMElement;
+}
+
+/*
+ * ADD TEXT & DELETE MARKS
+ */
+
+function add_Text_To_Task(text, taskDOMElement) {
+    let taskText = createTaskText(text, taskDOMElement.id);
+    taskDOMElement.appendChild(taskText);
+}
+
+function add_Delete_To_Task(taskDOMElement) {
+    let deleteMark = createDeleteMark(taskDOMElement.id);
+    taskDOMElement.appendChild(deleteMark);
+}
+
+function add_DeleteDone_To_DoneColumn() {
+    let doneDOMElement = document.getElementById("doneColumnTitle");
+    let deleteDoneMark = createDeleteDoneMark();
+    doneDOMElement.textContent = doneDOMElement.textContent + SEPARATOR;
+    doneDOMElement.appendChild(deleteDoneMark);
+}
+
+function add_DeleteAll_To_BoardTitle() {
+    let boardTitleDOMElement = document.getElementById("boardTitle");
+    let deleteAllMark = createDeleteAllMark();
+    boardTitleDOMElement.textContent = boardTitleDOMElement.textContent + SEPARATOR;
+    boardTitleDOMElement.appendChild(deleteAllMark);
+}
+
+/*
+ * SAVE TO AND LOAD TASKS FROM LOCAL STORAGE
+ */
+
 function saveTasks_To_LocalStorage() {
-    const taskList = [];
+    let taskList = [];
 
     document.querySelectorAll(".kanban-card").forEach(task => {
         taskList.push({
-            id: task.dataset.id,
+            id: task.id,
             className: task.className,
-            text: task.textContent,
+            text: task.children[0].textContent,
             columnId: task.parentElement.id
         });
-        console.log("saving " + task.textContent + " " + task.className);
     });
     document.querySelectorAll(".kanban-card-priority").forEach(task => {
         taskList.push({
-            id: task.dataset.id,
+            id: task.id,
             className: task.className,
-            text: task.textContent,
+            text: task.children[0].textContent,
             columnId: task.parentElement.id
         });
-        console.log("saving " + task.textContent + " " + task.className);
     });
 
     localStorage.setItem("tasks_in_localStorage", JSON.stringify(taskList));
@@ -62,7 +182,6 @@ function loadTasks_From_LocalStorage() {
     if (savedTasks) {
 
         savedTasks.forEach(({id, className, text, columnId}) => {
-           console.log("loaded " + text + " " + className);
            let isPriorityChecked = (className === "kanban-card-priority");
            const taskDOMElement = createTaskElement(text, isPriorityChecked, id);
            document.getElementById(columnId).appendChild(taskDOMElement);
@@ -73,8 +192,9 @@ function loadTasks_From_LocalStorage() {
 
 function startProgram() {
 
-    // localStorage.clear();
     loadTasks_From_LocalStorage();
+    add_DeleteAll_To_BoardTitle();
+    add_DeleteDone_To_DoneColumn();
 
     taskForm.addEventListener("submit", (e) => {
         e.preventDefault();
