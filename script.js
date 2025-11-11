b40081fecc82264c8269dd9b60ad8eb9d7caf581
Abhirup Dutta <abhirupdutta@Abhirups-MacBook-Pro.local>
2025-11-10 22:22:08 -0500

    persistent local storage on browser refresh

--- script.js
+++ script.js
@@ -24,21 +24,69 @@ function createTaskElement(text, isPriorityChecked, id = Date.now().toString()) 
     taskDOMElement.addEventListener("dragend", (e) => {
         draggedTask = null;
         taskDOMElement.classList.remove("hidden");
+
+        saveTasks_To_LocalStorage();
     });
 
     return taskDOMElement;
 }
 
+function saveTasks_To_LocalStorage() {
+    const taskList = [];
+
+    document.querySelectorAll(".kanban-card").forEach(task => {
+        taskList.push({
+            id: task.dataset.id,
+            className: task.className,
+            text: task.textContent,
+            columnId: task.parentElement.id
+        });
+        console.log("saving " + task.textContent + " " + task.className);
+    });
+    document.querySelectorAll(".kanban-card-priority").forEach(task => {
+        taskList.push({
+            id: task.dataset.id,
+            className: task.className,
+            text: task.textContent,
+            columnId: task.parentElement.id
+        });
+        console.log("saving " + task.textContent + " " + task.className);
+    });
+
+    localStorage.setItem("tasks_in_localStorage", JSON.stringify(taskList));
+}
+
+function loadTasks_From_LocalStorage() {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks_in_localStorage"));
+
+    if (savedTasks) {
+
+        savedTasks.forEach(({id, className, text, columnId}) => {
+           console.log("loaded " + text + " " + className);
+           let isPriorityChecked = (className === "kanban-card-priority");
+           const taskDOMElement = createTaskElement(text, isPriorityChecked, id);
+           document.getElementById(columnId).appendChild(taskDOMElement);
+        });
+
+    }
+}
+
 function startProgram() {
+
+    // localStorage.clear();
+    loadTasks_From_LocalStorage();
 
     taskForm.addEventListener("submit", (e) => {
         e.preventDefault();
+
         const taskText = taskInput.value.trim();
         if (taskText) {
             const taskElement = createTaskElement(taskText, isPriority.checked);
             document.getElementById("toDoColumn").appendChild(taskElement);
             taskInput.value = "";
         }
+
+        saveTasks_To_LocalStorage();
     });
 
 
@@ -53,9 +101,12 @@ function startProgram() {
 
         column.addEventListener("drop", (e) => {
             e.preventDefault();
+
             if (draggedTask) {
                 column.appendChild(draggedTask);
             }
+
+            saveTasks_To_LocalStorage();
         });
 
     });
